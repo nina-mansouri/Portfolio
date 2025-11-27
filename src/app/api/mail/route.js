289e68c27c81344548a7bd17ee1c60f26b10f94d
nina <n.mansouri2486@gmail.com>
2025-11-27 23:02:18 +0330

    email commit

--- src/app/api/mail/route.js
+++ src/app/api/mail/route.js
@@ -1,14 +1,14 @@
 import nodemailer from "nodemailer";
 
-// import connectDB from "@/utils/connectDB";
-// import { Mail } from "@/models/mail";
+import connectDB from "@/utils/connectDB";
+import { Mail } from "@/models/mail";
 
 export async function POST(req) {
   try {
     const body = await req.json();
     
     const { name, email, subject, message } = body.formData;
-    console.log("received:", name, email, subject, message);
+    
 
     const transporter = nodemailer.createTransport({
       service: "Gmail",
@@ -27,8 +27,8 @@ export async function POST(req) {
     // console.log("mail options:", mailOptions)
     await transporter.sendMail(mailOptions);
 
-    // connectDB();
-    // const mail = await Mail.create(body.formData);
+    connectDB();
+    const mail = await Mail.create(body.formData);
     return new Response(
       JSON.stringify({ success: true, message: "Email sent successfully" }),
       { status: 201 }
